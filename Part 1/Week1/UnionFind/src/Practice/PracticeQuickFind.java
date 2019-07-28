e7a881511962a1c77c15aa00b62da77f480e9616
Jesse A <jcazub@gmail.com>
2019-07-28 03:05:45 -0400

    continued with week 1 practices

---
+++ Part 1/Week1/UnionFind/src/Practice/PracticeQuickFind.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class PracticeQuickFind {
+
+    private int[] id;
+
+    public PracticeQuickFind(int N) {
+        id = new int[N];
+
+        for (int i = 0; i < id.length; i++) {
+            id[i] = i;
+        }
+    }
+
+    public boolean find(int p, int q) {
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q) {
+
+        int pid = id[p];
+        int qid = id[q];
+
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == qid) id[i] = pid;
+        }
+
+    }
+}
