4df8bae69809385447053695aab0062f3ede96ec
Jesse Azubuike <jesseazubuike@Jesses-MacBook-Pro.local>
2020-03-27 09:43:30 -0400

    started working on Board assignment

---
+++ Assignments/Puzzle/src/Board.java
@@ -0,0 +1,42 @@
+public final class Board {
+
+    public Board(int[][] tiles) {
+
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    public int dimension() {
+        return -1;
+    }
+
+    public int hamming() {
+        return -1;
+    }
+
+    public int manhattan() {
+        return -1;
+    }
+
+    public boolean isGoal() {
+        return false;
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    public Board twin() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // used for testing
+    }
+}
