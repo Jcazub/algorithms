ed05dce8e60ed826b38fd0b870486d2f47575d7c
Jesse A <jcazub@gmail.com>
2020-08-05 10:07:16 -0400

    finished implementation for PointSET

--- Assignments/KdTree/src/PointSET.java
+++ Assignments/KdTree/src/PointSET.java
@@ -1,38 +1,73 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.ArrayList;
 
 public class PointSET {
 
-    public PointSET() {
+    private SET<Point2D> points;
 
+    public PointSET() {
+        points = new SET<>();
+    }
+
+    public boolean isEmpty() {
+        return points.isEmpty();
     }
 
     public int size() {
-        return -1;
+        return points.size();
     }
 
     public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
 
+        points.add(p);
     }
 
     public boolean contains(Point2D p) {
-        return false;
+        if (p == null) throw new IllegalArgumentException();
+
+        return points.contains(p);
     }
 
     public void draw() {
-
+        for (Point2D point : points) {
+            point.draw();
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        if (rect == null) throw new IllegalArgumentException();
+
+        ArrayList<Point2D> insideRectangle = new ArrayList<>();
+
+        for (Point2D point : points) {
+            if (rect.contains(point)) insideRectangle.add(point);
+        }
+
+        return insideRectangle;
     }
 
     public Point2D nearest(Point2D p) {
-        return null;
+        if (p == null) throw new IllegalArgumentException();
+
+        if (this.isEmpty()) return null;
+
+        Point2D nearestNeighbor = null;
+        Double nearestNeighborDistance = Double.POSITIVE_INFINITY;
+
+        for (Point2D point : points) {
+            Double currentDistance = p.distanceTo(point);
+
+            if (Double.compare(currentDistance, nearestNeighborDistance) < 0) {
+                nearestNeighbor = point;
+                nearestNeighborDistance = currentDistance;
+            }
+        }
+
+        return nearestNeighbor;
     }
-
-    public static void main(String[] args) {
-
-    }
-
+    
 }
