9ff8cbbc8bb61b2f130499cd027544f4d4767af8
Jesse A <jcazub@gmail.com>
2020-08-06 23:31:09 -0400

    added implementations of nearest and range for kdtree

--- Assignments/KdTree/src/KdTree.java
+++ Assignments/KdTree/src/KdTree.java
@@ -3,6 +3,9 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.RedBlackBST;
 import edu.princeton.cs.algs4.SET;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KdTree {
 
     private Node root;
@@ -104,16 +107,88 @@ public class KdTree {
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null) throw new IllegalArgumentException();
 
-        return null;
+        ArrayList<Point2D> insideRectangle = new ArrayList<>();
+
+        range(root, rect, insideRectangle);
+
+        return insideRectangle;
+    }
+
+    private void range(Node node, RectHV rect, List<Point2D> insideRectangle) {
+
+        if (node == null || !rect.intersects(node.rect)) return;
+        if (rect.contains(node.p)) insideRectangle.add(node.p);
+
+        range(node.lb, rect, insideRectangle);
+        range(node.rt, rect, insideRectangle);
+
     }
 
     public Point2D nearest(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
 
-        return null;
+        if (!contains(p)) return null;
+
+        return nearest(root, p, null, true);
+    }
+
+    private Point2D nearest(Node node, Point2D p, Point2D closestPoint, boolean isVertical) {
+
+        if (closestPoint == null) closestPoint = node.p;
+
+        Double closestSoFarDistance = p.distanceSquaredTo(closestPoint);
+
+        Double possibleNewClosestDistance = p.distanceSquaredTo(node.p);
+
+        if (Double.compare(closestSoFarDistance, possibleNewClosestDistance) > 0) {
+            closestPoint = node.p;
+            closestSoFarDistance = p.distanceSquaredTo(closestPoint);
+        }
+
+        Double distanceToLbRect = node.lb.rect.distanceSquaredTo(p);
+        Double distanceToRTRect = node.rt.rect.distanceSquaredTo(p);
+
+        boolean isLbRectCloser = Double.compare(closestSoFarDistance, distanceToLbRect) >= 0;
+        boolean isRbRectCloser = Double.compare(closestSoFarDistance, distanceToRTRect) >= 0;
+
+        if (isLbRectCloser && isRbRectCloser) {
+            if (isVertical) {
+                boolean isPointSmaller = Double.compare(p.x(), node.p.x()) < 0;
+
+                if (isPointSmaller) {
+                    nearest(node.lb, p, closestPoint, false);
+                    nearest(node.rt, p, closestPoint, false);
+                } else {
+                    nearest(node.rt, p, closestPoint, false);
+                    nearest(node.lb, p, closestPoint, false);
+                }
+            } else {
+                boolean isPointSmaller = Double.compare(p.y(), node.p.y()) < 0;
+
+                if (isPointSmaller) {
+                    nearest(node.lb, p, closestPoint, false);
+                    nearest(node.rt, p, closestPoint, false);
+                } else {
+                    nearest(node.rt, p, closestPoint, false);
+                    nearest(node.lb, p, closestPoint, false);
+                }
+            }
+
+        } else if (isLbRectCloser) {
+            nearest(node.lb, p, closestPoint, !isVertical);
+        } else if (isRbRectCloser) {
+            nearest(node.rt, p, closestPoint, !isVertical);
+        }
+
+        return closestPoint;
+
     }
 
     public static void main(String[] args) {
+
+    }
+
+    private void testInsert() {
         KdTree kd = new KdTree();
 
         Point2D p1 = new Point2D(0.5, 0.5);
@@ -129,5 +204,7 @@ public class KdTree {
         System.out.println(kd.contains(new Point2D(0.4, 0.4)));
         System.out.println(kd.size());
     }
+
+    private void testCl
 
 }
